21fecd79a3e73078e66683372e180bae20491a68
Jaime <jaime_neckel-junior@estudante.sesisenai.org.br>
2025-09-23 00:28:33 +0000

    Refatora o sistema de metas pessoais para usar inquirer e melhorar a interação do usuário

--- index.js
+++ index.js
@@ -1,27 +1,74 @@
+const { input, select } = require('@inquirer/prompts');
 
 console.log("=== Sistema de Metas Pessoais ===");
 console.log("Bem-Vindo ao sistema de metas pessoais!");
 
 let metas =[];
 
-console.log("Metas Atuais:", metas);
+async function mostrarMenu() {
+    const opcao = await select({
+        message: "Escolha uma opção:",
+        choices: [
+            { name: "Adicionar Meta", value: "adicionar" },
+            { name: "Mostrar Metas", value: "mostrar" },
+            { name: "Sair", value: "sair" }
+        ]
+    });
+    return opcao;
+}
 
-const inquirer = require('inquirer');
+async function executarAcao(opcao) {
+    switch (opcao) {
+        case "adicionar":
+            await adicionarMeta();
+            break;
+        case "mostrar":
+            await mostrarMetas();
+            break;
+        case "sair":
+            console.log("Saindo do sistema. Até mais!");
+            process.exit(0);
+        default:
+            console.log("Opção inválida. Tente novamente.");
+    }
+}
 
-function adicionarMeta() {
-inquirer.input([
-    {
-      type: 'input',
-      name: 'novaMeta',
-      message: 'Digite sua nova meta:',
-    },
-  ]);
-  then((answers) => {
-    metas.push(answers.novaMeta);
-    console.log("Meta Adicionada:", novaMeta);
-    console.log("Metas Atualizadas:", metas.length);
-  });
-};
+async function iniciar() {
+    while (true) {
+        const opcao = await mostrarMenu();
+        
+        if (opcao === "sair") {
+            await executarAcao(opcao);
+            break;
+    }
+    await executarAcao(opcao);
+ }
+}
 
-adicionarMeta(metas);
-console.log("Metas Finais:", metas);
+
+async function adicionarMeta() {
+    let novaMeta = await input({
+        message: "Digite sua nova meta pessoal:"
+    });
+
+    if (novaMeta.length === 0) {
+        console.log("❌ Meta inválida. Tente novamente.");
+        return;
+    }
+    metas.push(novaMeta);
+
+    console.log("✔️ Meta adicionada com sucesso!");
+}
+
+async function mostrarMetas() {
+    console.log("Suas Metas Pessoais:");
+    metas.forEach((meta, index) => {
+        console.log(`${index + 1}. ${meta}`);
+    });
+}
+
+iniciar();
+
+
+
+

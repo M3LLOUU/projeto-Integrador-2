79b592853b0bd63b5ddd627c9fe06154f8465db1
Jaime <jaime_neckel-junior@estudante.sesisenai.org.br>
2025-09-24 23:59:47 +0000

    Adiciona funcionalidade para marcar metas como realizadas e melhora a interação do usuário

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
-const { input, select } = require('@inquirer/prompts');
+const { input, select, checkbox } = require('@inquirer/prompts');
 
-console.log("=== Sistema de Metas Pessoais ===");
 console.log("Bem-Vindo ao sistema de metas pessoais!");
 
 let metas =[];
 
+function limapTela() {
+
+    console.clear();
+}
+
+function mostrarMensagem(mensagem) {
+
+    console.log(`\n${mensagem}\n`);
+}
+
 async function mostrarMenu() {
+
     const opcao = await select({
         message: "Escolha uma opção:",
         choices: [
-            { name: "Adicionar Meta", value: "adicionar" },
-            { name: "Mostrar Metas", value: "mostrar" },
-            { name: "Sair", value: "sair" }
+            { name: "📝 Adicionar Meta", value: "adicionar" },
+            { name: "🗒️  Mostrar Metas", value: "mostrar" },
+            { name: "✅ Marcar metas como realizadas", value: "marcar"},
+            { name: "❌ Sair", value: "sair" }
         ]
     });
     return opcao;
 }
 
 async function executarAcao(opcao) {
+
     switch (opcao) {
         case "adicionar":
             await adicionarMeta();
             break;
         case "mostrar":
             await mostrarMetas();
+            break;
+        case "marcar":
+            await marcarMetas();
             break;
         case "sair":
             console.log("Saindo do sistema. Até mais!");
@@ -34,39 +49,81 @@ async function executarAcao(opcao) {
 }
 
 async function iniciar() {
+
+    limapTela();
+    mostrarMensagem("=== Sistema de Metas Pessoais ===");
+
     while (true) {
         const opcao = await mostrarMenu();
         
         if (opcao === "sair") {
             await executarAcao(opcao);
+            limparTela();
+            mostrarMensagem("Até mais! 👋");
             break;
     }
     await executarAcao(opcao);
  }
 }
 
-
 async function adicionarMeta() {
-    let novaMeta = await input({
+
+    const descricao = await input({
         message: "Digite sua nova meta pessoal:"
     });
 
-    if (novaMeta.length === 0) {
-        console.log("❌ Meta inválida. Tente novamente.");
+    if (descricao.length === 0) {
+        mostrarMensagem("❌ Meta inválida. Tente novamente.");
         return;
     }
-    metas.push(novaMeta);
 
-    console.log("✔️ Meta adicionada com sucesso!");
+    const novaMeta = {
+        value: descricao,
+        checked: false
+    }
+
+    metas.push(novaMeta);
+    mostrarMensagem("✔️  Meta adicionada com sucesso!");
 }
 
 async function mostrarMetas() {
+
+    if (metas.length === 0) {
+        mostrarMensagem("🔻 Não existem mais metas cadastradas! 🔻");
+        return;
+    }
+
     console.log("Suas Metas Pessoais:");
     metas.forEach((meta, index) => {
-        console.log(`${index + 1}. ${meta}`);
+        const status = meta.checked ? "[x]" : "[ ]";
+        console.log(`${status} ${index + 1}. ${meta.value}`);
     });
 }
 
+async function marcarMetas(){
+
+    if (metas.length === 0){
+        mostrarMensagem(" Não existem metas cadastradas");
+        return;
+    }
+    
+    const metasSelecionadas = await checkbox({
+        message: "Selecione as metas que você já concluiu:",
+        choices: metas.map (meta => 
+        ({ name: meta.value,
+            value: meta.value,
+            checked: meta.value
+        })),
+    })
+
+    metasSelecionadas.forEach(metasSelecionadas => {
+        const meta = metas.find(m => m.value === metasSelecionadas)
+        if (meta) {
+            meta.checked = true;
+        }
+    });
+    mostrarMensagem(" ✅ Metas atualizadas com sucesso!");
+}
 iniciar();
 
 
